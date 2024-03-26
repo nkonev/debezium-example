cb216bba9e1f490d1ce1d99dbc479d8dc961fe7b
Конев Никита Дмитриевич <nkonev@samokat.ru>
2024-03-26 09:37:21 +0300

    move create scripts to the dedicated folder & eliminate escaping

---
+++ docker/create-debezium-connectors/create.sh
@@ -0,0 +1,16 @@
+set -e
+echo 'creating postgres connector'
+http_code=$(curl -Ss -o /dev/null -w '%{http_code}' --connect-timeout 5 --max-time 5 -X POST -H 'Accept:application/json' -H 'Content-Type:application/json' --url 'http://connect:8083/connectors/' -d @/opt/create-debezium-connectors/connector-postgres.json)
+echo "http_code=$http_code"
+if [ "$http_code" != "201" -a "$http_code" != "409" ]; then
+  echo "exit because of wrong code during creating postgres connector";
+  exit 1;
+fi
+
+echo 'creating clickhouse connector'
+http_code=$(curl -Ss -o /dev/null -w '%{http_code}' --connect-timeout 5 --max-time 5 -X POST -H 'Accept:application/json' -H 'Content-Type:application/json' --url 'http://connect:8083/connectors/' -d @/opt/create-debezium-connectors/connector-clickhouse.json)
+echo "http_code=$http_code"
+if [ "$http_code" != "201" -a "$http_code" != "409" ]; then
+  echo "exit because of wrong code during creating clickhouse connector";
+  exit 1;
+fi
